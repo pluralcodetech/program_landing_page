d3c0a1a6325b69a2627d99a33658d854c2a76b02
Vchuks <64593598+Vchuks@users.noreply.github.com>
2023-05-30 16:44:14 +0100

    update

--- src/components/atom/Sidebar.js
+++ src/components/atom/Sidebar.js
@@ -1,33 +1,36 @@
 import React from 'react'
-import { NavLink } from "react-router-dom";
+import { Link } from "react-scroll";
 import Button from "./Button";
 
 const Sidebar = () => {
   return (
       <div className="w-100 fw-semibold ab ">
       <div className="navbtntwo d-flex flex-column gap-3">
-        <NavLink to="" className="text-decoration-none navtextcolor">
+      <Link to="cert" smooth={true} className="text-decoration-none navtextcolor">
           Certificate Courses
-        </NavLink>
-        <NavLink to="" className="text-decoration-none navtextcolor">
-          Why Pluralcode?
-        </NavLink>
-        <NavLink to="" className="text-decoration-none navtextcolor">
+        </Link>
+        <Link to="cert" smooth={true} className="text-decoration-none navtextcolor">
+          Diploma Courses
+        </Link>
+        <Link to="tuit" smooth={true} className="text-decoration-none navtextcolor">
+          Tuition Fees
+        </Link>
+        <Link to="accredit" smooth={true} className="text-decoration-none navtextcolor">
           Testimonies
-        </NavLink>
-        <NavLink to="" className="text-decoration-none navtextcolor">
+        </Link>
+        <Link to="accredit" smooth={true} className="text-decoration-none navtextcolor">
           Accreditation
-        </NavLink>
-        <NavLink to="" className="text-decoration-none navtextcolor">
-          FAQ
-        </NavLink>
+        </Link>
       </div>
       <div className="navbtnone d-flex gap-4 mt-4">
-        <Button
+      <Link to="cert" smooth={true}>
+        <Button children="Begin Enrollment" className="w-100 pbcolor btn button" />
+      </Link>
+        {/* <Button
           children="Speak to Advisor"
           className="ptcolor px-4 btn pborder"
         />
-        <Button children="Enroll Now" className="pbcolor btn px-4 button" />
+        <Button children="Enroll Now" className="pbcolor btn px-4 button" /> */}
       </div>
     </div>
   )
